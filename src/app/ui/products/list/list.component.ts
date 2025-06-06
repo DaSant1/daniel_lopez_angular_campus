b0320d05dcc22918ebf745845b6bf1500ec4ec5a
el Daniel <santi.lopez10889@gmail.com>
2025-06-05 21:03:28 -0500

    [DS]-desktop-view-done

--- src/app/ui/products/list/list.component.ts
+++ src/app/ui/products/list/list.component.ts
@@ -1,11 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../../shared/services/product.service';
 import { product_model } from '../../shared/models/product.model';
-
+import { CommonModule } from '@angular/common';
+import { MatTableModule, MatTableDataSource } from '@angular/material/table';
+import { MatPaginatorModule, MatPaginator } from '@angular/material/paginator';
+import { MatSortModule, MatSort } from '@angular/material/sort';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIcon, MatIconModule } from '@angular/material/icon';
+import { MatDialogModule, MatDialog } from '@angular/material/dialog';
+import { MatSnackBarModule, MatSnackBar } from '@angular/material/snack-bar';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatCardModule } from '@angular/material/card';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatInputModule,
+    MatSnackBarModule,
+    MatCardModule,
+    MatChipsModule,
+    MatIconModule
+  ],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
@@ -17,7 +42,7 @@ export class ListComponent implements OnInit{
     this.productService.getAllProducts().then((products) => this.products = products);
   }
 
-  
+
 
 
 }
